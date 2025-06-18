ca07968359e02ced81ba19108d9276107f20d329
Timothy Dang <timothy.dang@gmail.com>
2025-06-18 16:02:50 -0700

    add

---
+++ frontend/src/index.ts
@@ -0,0 +1,10 @@
+import { User } from "./types";
+
+/**
+ * If possible, put your minimal reproduction in this file, but feel free to
+ * create other files elsewhere in the project if necessary.
+ *
+ * Once you have a minimal reproduction, commit it and push it to a public
+ * GitHub repo.
+ */
+export const someFunction = (input: string): User => ({ name: input });
